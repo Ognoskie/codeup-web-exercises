097707070f08762d07aa99283dadb0531f9fb12a
Braden Wittkop <bradenjwittkop@gmail.com>
2023-06-12 09:47:19 -0500

    added warm ups for today and edit welcome html

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -33,17 +33,46 @@ makeUpperCase(true) // returns false
 makeUpperCase(null) // returns false
 
 */
-function makeUpperCase(input) {
-        if (typeof input === "string" ) {
-            return input.toUpperCase();
-        }else{
-            return false;
-        }
-}
+// function makeUpperCase(input) {
+//         if (typeof input === "string" ) {
+//             return input.toUpperCase();
+//         }else{
+//             return false;
+//         }
+// }
+//
+// console.log(makeUpperCase('cat')); // returns 'CAT'
+// console.log(makeUpperCase('DoG')); // returns 'DOG'
+// console.log(makeUpperCase('CODEUP')); // returns 'CODEUP'
+// console.log(makeUpperCase(54)); // returns false
+// console.log(makeUpperCase(true)); // returns false
+// console.log(makeUpperCase(null)); // returns false
 
-console.log(makeUpperCase('cat')); // returns 'CAT'
-console.log(makeUpperCase('DoG')); // returns 'DOG'
-console.log(makeUpperCase('CODEUP')); // returns 'CODEUP'
-console.log(makeUpperCase(54)); // returns false
-console.log(makeUpperCase(true)); // returns false
-console.log(makeUpperCase(null)); // returns false
+
+
+// function sumIsEqual(input1, input2, input3) {
+//     if (input1 + input2 === input3) {
+//         return true
+//     } else{
+//         return false
+//     }
+// }
+
+console.log(sumIsEqual(1, 2, 3));
+console.log(sumIsEqual(2, 2, 4));
+console.log(sumIsEqual(10, 2, 12));
+console.log(sumIsEqual(10, false, 12));
+console.log(sumIsEqual("10", "2", "12"));
+console.log(sumIsEqual("fred", "bob", "hello"));
+
+
+// walk through
+function sumIsEqual(input1, input2, input3) {
+    if (typeof input1 !== "number" || typeof input2 !== "number" || typeof input3 !== "number" ) {
+        return false
+    } else if (input1 + input2 === input3){
+        return true;
+    } else {
+        return false
+    }
+}
