571cb25ddb7ed0b99a701dcce75f963d1bf5b7fd
Braden Wittkop <bradenjwittkop@gmail.com>
2023-06-30 09:06:42 -0500

    added dom exercise

---
+++ js/dom-query-js.js
@@ -0,0 +1,23 @@
+(function() {
+    "use strict";
+    // TODO: Uncomment next line and get the main header element by id
+    var mainHeader =
+
+    // TODO: Set inner html of mainHeader to "JavaScript is Cool"
+
+    // TODO: Uncomment the next line and get the sub header element by id
+    // var subHeader = ;
+
+    // TODO: Set the text color of subHeader to blue
+
+    // TODO: Uncomment the next line and get all list items
+    // var listItems = ;
+
+    // TODO: Set text color on every other list item to grey
+    // TODO: Set text color of li with dbid = 1 to blue
+
+    // TODO: Uncomment the next line and get all elements with class name sub-paragraph
+    // var subParagraphs = ;
+
+    // TODO: Change the text in the first sub paragraph to "Mission Accomplished!"
+})();
