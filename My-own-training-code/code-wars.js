764cd6fb709d8bc440a2f042182e1a03cbffb99f
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-03 22:37:57 -0500

    added to code wars file, dom, and my own code

--- My-own-training-code/code-wars.js
+++ My-own-training-code/code-wars.js
@@ -5,7 +5,23 @@
 // Note: The function accepts an integer and returns an integer.
 
 
-function squareDigits(num){
-    return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
+// function squareDigits(num){
+//     return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
+//
+// }
 
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+
+function createArray(number){
+    let newArray = [];
+
+    for(let counter = 1; counter <= number;){
+        newArray.push(counter);
+    }
+
+    return newArray;
 }
+
+console.log(createArray())
