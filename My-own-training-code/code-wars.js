9c569eca1b21420bc3137fdcd1bbb6a367efaaee
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-04 20:50:23 -0500

    debugging code in code wars file

--- My-own-training-code/code-wars.js
+++ My-own-training-code/code-wars.js
@@ -17,7 +17,7 @@
 function createArray(number){
     let newArray = [];
 
-    for(let counter = 1; counter <= number;){
+    for(let counter = 1; counter >= number;){
         newArray.push(counter);
     }
 
