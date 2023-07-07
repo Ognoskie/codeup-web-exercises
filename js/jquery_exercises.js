ae268b62e442596c3477e87bdc7da2bdd0c9f660
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-07 16:28:08 -0500

    added jQuery exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -11,8 +11,77 @@
 // jQuery === $
 
 
- $(function () {
-    alert("The window has loaded");
- });
+ // $(function () {
+ //    alert("The window has loaded");
+ // });
+
+// jquery selectors notes
+$(() => {
+// // calling element by id
+//     $('#id-container').css('background-color', 'red')
+//
+// // calling element by name
+//     $('.title').css('color', 'purple');
+//
+//
+//     // same with js
+//     let titleElements = document.querySelectorAll('.title');
+//     titleElements.forEach(titleElements => {
+//         titleElements.style.color = 'blue'
+//     });
+//
+//
+// //use jquery to select all 'li' elements
+//     $('li').css('color', 'pink');
+//
+//     // multiple selector in $
+//     $('h1, p, .highlight').css('font-weight', 'bold')
+//
+//     // using jquery to check for a value
+//     console.log($('[data-id="5"]').html());
+//
+//
+// //select everything
+//     $('*').css('outline', '1px solid lightblue').css('font-weight', 'bold');
+
+$('.codeup').css('border', '1px solid red');
+
+$('li').css('font-size', '20px')
+
+    $('h1, p, li').css('background-color', '#FFFF00');
 
 
+
+$('#title').html()
+    alert("Ive loaded up!!!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
