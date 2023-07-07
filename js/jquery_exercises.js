7d9d9ffac7ad886c813831179dd0a258881c821a
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-07 14:00:00 -0500

    added jQuery exercise

---
+++ js/jquery_exercises.js
@@ -0,0 +1,18 @@
+// window.onload = function () {
+//     alert("The window has loaded");
+// }
+
+
+
+// jQuery(function () {
+//     alert("The window has loaded");
+// });
+
+// jQuery === $
+
+
+ $(function () {
+    alert("The window has loaded");
+ });
+
+
