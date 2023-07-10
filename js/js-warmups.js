5c88036267679996f67e54891d8ecef88af37b98
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-10 10:02:02 -0500

    js warm up

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -532,20 +532,113 @@ returnAdmins(users) returns...
 
     */
 // this code was fun
-function fizzBuzz() {
-    for (let i = 1; i <= 100; i++){
-        if (i % 3 === 0 && i % 5 === 0){
-            console.log("fizzbuzz");
-        } else if (i % 5 === 0){
-            console.log("buzz");
-        }else if (i % 3 === 0){
-            console.log("fizz")
-        }else{
-            console.log(i)
+// function fizzBuzz() {
+//     for (let i = 1; i <= 100; i++){
+//         if (i % 3 === 0 && i % 5 === 0){
+//             console.log("fizzbuzz");
+//         } else if (i % 5 === 0){
+//             console.log("buzz");
+//         }else if (i % 3 === 0){
+//             console.log("fizz")
+//         }else{
+//             console.log(i)
+//         }
+//     }
+// }
+// fizzBuzz();
+
+
+
+
+// You are given an array of runner objects, where each object represents a
+// runner with properties name, age, and lapTimes (in seconds). Write a
+// function called getTopRunners that takes the runners array as input and
+// returns an array of top runners. A runner is considered top if their
+// average lap time is 50 seconds or faster. If there are no top runners in
+// the input array, the function should return an empty array
+
+
+// HINT: You might want to create a function called calculateLapTimeAverage
+// that takes in an array of numbers and returns the average
+
+
+// Input:
+const runners = [
+    { name: "Alice", age: 25, lapTimes: [50.5, 45.2, 48.8, 47.1],
+
+    },
+    { name: "Bob", age: 28, lapTimes: [55.2, 50.7, 53.3, 52.9] },
+    { name: "Charlie", age: 24, lapTimes: [48.9, 46.3, 50.1, 49.5] },
+    { name: "David", age: 27, lapTimes: [51.4, 50.2, 49.8, 52.3] },
+    { name: "Eve", age: 26, lapTimes: [47.7, 45.9, 46.6, 48.3] },
+];
+
+
+
+function getTopRunners () {
+    for (let i = 0; i < runners.length; i++) {
+        const sum = runners[i].lapTimes.reduce((acc, current) => acc + current, 0);
+        const average = sum / runners.length;
+
+        if (average < 50) {
+            return runners.splice(i);
         }
     }
+
 }
-fizzBuzz();
+
+console.log(getTopRunners());
+
+
+
+
+
+
+
+// Expected Output:
+// [
+//     { name: 'Alice', age: 25, lapTimes: [50.5, 45.2, 48.8, 47.1] },
+//     { name: 'Charlie', age: 24, lapTimes: [48.9, 46.3, 50.1, 49.5] },
+//     { name: 'Eve', age: 26, lapTimes: [47.7, 45.9, 46.6, 48.3] }
+// ]
+//
+//
+//
+// // BONUS: Each runner in the resulting array should be represented as an
+// // object containing only the name and age properties.
+//
+//
+// // Expected Bonus Output:
+//     [
+//     { name: "Alice", age: 18 },
+//         { name: "Charlie", age: 19 },
+//         { name: "Eve", age: 17 }
+//     ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
