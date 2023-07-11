515f2f0be6c5dd28fb3033827762685feb8741e5
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-11 17:16:38 -0500

    added filter and map method for arrays

--- My-own-training-code/js-book-array.js
+++ My-own-training-code/js-book-array.js
@@ -7,10 +7,21 @@ const printStuff = (element, index) => {
 }
 arr.forEach(printStuff);
 
-// filter method for an array pg. 177
+// filter method for an array pg.177
 
+let ar = ["squirrel", 5, "Tjed", new Date(), true];
 
+const checkString = (element, index) => {
+   return typeof element === "string"
+}
+let filterAr = ar.filter(checkString);
+console.log(filterAr);
 
+// map method for an array pg.179
+
+let nums = [1, 2, 3, 4];
+let mapped_arr = nums.map(x => x + 5);
+console.log(mapped_arr)
 
 
 
