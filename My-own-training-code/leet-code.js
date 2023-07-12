3de1af86d729e9b32c24f0ed82839a0a6b145f0e
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-12 09:03:09 -0500

    leet code problems

--- My-own-training-code/leet-code.js
+++ My-own-training-code/leet-code.js
@@ -27,8 +27,101 @@
 // At most 1000 calls to counter() will be made
 
 
-let counter = 0
-let createCounter = function(n) {
-    counter++
-    console.log(counter)
+// let counter = 0
+// let createCounter = function(n) {
+//     counter++
+//     console.log(counter)
+// }
+
+
+
+
+
+
+
+
+
+// problem 200 number of islands
+
+
+// Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+//
+//     An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+//
+//
+//
+//     Example 1:
+//
+// Input: grid = [
+//     ["1","1","1","1","0"],
+//     ["1","1","0","1","0"],
+//     ["1","1","0","0","0"],
+//     ["0","0","0","0","0"]
+// ]
+// Output: 1
+// Example 2:
+//
+// Input: grid = [
+//     ["1","1","0","0","0"],
+//     ["1","1","0","0","0"],
+//     ["0","0","1","0","0"],
+//     ["0","0","0","1","1"]
+// ]
+// Output: 3
+
+//
+// /**
+//  * @param {character[][]} grid
+//  * @return {number}
+//  */
+
+
+const dFS = (i, j, grid, visited) => {
+    const stack = [[i, j]];
+    let islandSize = 0;
+
+
 }
+
+const numIslands = function(grid) {
+    const visited = grid.map(row => row.map(cell => false));
+    let islandCount = 0;
+
+
+    for(let i = 0; i < grid.length; i++) {
+        for(let j = 0; j < grid[j].length; j++) {
+            if (dFS(i, j, grid, visited)) islandCount++;
+        }
+    }
+
+
+
+
+
+
+    return islandCount
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
