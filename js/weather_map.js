9b7c7047f562c69028e2097399d611aef1af0a2f
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-16 00:11:32 -0500

    put the map on the page

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,46 +1,47 @@
-"use strict";
+$(() => {
+    "use strict";
 
 // Base URL for forecast API
-const OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast';
+    const OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 
 // Simple way to create URL for request based on coordinates
-function getWeatherURL(lat, lon) {
-    return `${OPEN_WEATHER_URL}?lat=${lat}&lon=${lon}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
-}
+    function getWeatherURL(lat, lon) {
+        return `${OPEN_WEATHER_URL}?lat=${lat}&lon=${lon}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
+    }
 
-const ALAMO_COORDINATES = ['29.4260', '-98.4861'];
+    const ALAMO_COORDINATES = ['29.4260', '-98.4861'];
 
 // TODO: log URL
 
 // console.log(getWeatherURL(ALAMO_COORDINATES[0], ALAMO_COORDINATES[1]));
 
-const URL = getWeatherURL(...ALAMO_COORDINATES);
+    const URL = getWeatherURL(...ALAMO_COORDINATES);
 
 // TODO: log full response from API
 
-$.ajax(URL).done(data => {
-    console.log(data);
-}).fail(console.error);
+    $.ajax(URL).done(data => {
+        console.log(data);
+    }).fail(console.error);
 
 
 // TODO: log various parts of the API
 
-$.ajax(URL).done(data => {
-    console.log(data);
-    // TODO: log the city name
-    console.log(data.city.name)
-    // TODO: log the first three-hour forecast block
-    console.log(data.list[0])
-    // TODO: log the humidity for the first three-hour block
-    console.log(data.list[0].main.humidity
-    )
-}).fail(console.error);
+    $.ajax(URL).done(data => {
+        console.log(data);
+        // TODO: log the city name
+        console.log(data.city.name)
+        // TODO: log the first three-hour forecast block
+        console.log(data.list[0])
+        // TODO: log the humidity for the first three-hour block
+        console.log(data.list[0].main.humidity
+        )
+    }).fail(console.error);
 
 
 // TODO: log the humidity for all days
 
-$.ajax(getWeatherURL(...ALAMO_COORDINATES))
-    .done((data) => {
+    $.ajax(getWeatherURL(...ALAMO_COORDINATES))
+        .done((data) => {
 //
 //         data.list.forEach((day, index) => {
 //             if (index % 8 === 0) {
@@ -50,25 +51,25 @@ $.ajax(getWeatherURL(...ALAMO_COORDINATES))
 //
 //         // OR
 //
-        for (let i = 0; i < data.list.length; i += 8) {
-            console.log(data.list[i].main.humidity);
-        }
+            for (let i = 0; i < data.list.length; i += 8) {
+                console.log(data.list[i].main.humidity);
+            }
 
-    })
-    .fail(console.error);
+        })
+        .fail(console.error);
 
 
 // TODO: log the min and max temp for each day
 
-$.ajax(getWeatherURL(...ALAMO_COORDINATES))
-    .done(data => {
-        console.log(data);
-        const minMaxTemps = returnMinMaxTemps(data);
-        minMaxTemps.forEach(minMaxTemp => {
-            console.log(minMaxTemp);
-        });
-    })
-    .fail(console.error);
+    $.ajax(getWeatherURL(...ALAMO_COORDINATES))
+        .done(data => {
+            console.log(data);
+            const minMaxTemps = returnMinMaxTemps(data);
+            minMaxTemps.forEach(minMaxTemp => {
+                console.log(minMaxTemp);
+            });
+        })
+        .fail(console.error);
 
 ////////////////////////////////////////////My code for weather map project////////////////////////////////////
 
@@ -87,9 +88,9 @@ $.ajax(getWeatherURL(...ALAMO_COORDINATES))
 
 
 // Global
-const map = startMap();
-const marker = createMarker();
-const popup = createPopup();
+    const map = startMap();
+    const marker = createMarker();
+    const popup = createPopup();
 
 
 
@@ -116,31 +117,31 @@ const popup = createPopup();
 
 //Functions
 
-function startMap() {
-    mapboxgl.accessToken = MAPBOX_STEVE_TOKEN;
+    function startMap() {
+        mapboxgl.accessToken = MAPBOX_STEVE_TOKEN;
 
-    const mapOptions = {
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v12',
-        zoom: 10,
-        center: [-98.4916, 29.4252],
+        const mapOptions = {
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v12',
+            zoom: 10,
+            center: [-98.4916, 29.4252],
+        }
+
+        return new mapboxgl.Map(mapOptions);
     }
 
-    return new mapboxgl.Map(mapOptions);
-}
-
-function createMarker() {
-    return new mapboxgl.Marker()
-    // .setLngLat([-98.4916, 29.4252])
-    // .addTo(map)
-}
+    function createMarker() {
+        return new mapboxgl.Marker()
+        // .setLngLat([-98.4916, 29.4252])
+        // .addTo(map)
+    }
 
 
-function createPopup() {
-    return new mapboxgl.Popup()
-    // .setLngLat([-98.4916, 29.4252])
+    function createPopup() {
+        return new mapboxgl.Popup()
+        // .setLngLat([-98.4916, 29.4252])
 
-}
+    }
 
 
 
@@ -227,3 +228,11 @@ function createPopup() {
 
 
 
+
+
+
+
+
+
+
+})
