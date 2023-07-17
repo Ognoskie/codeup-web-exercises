d92dfaad259d3201bef6a707abbdf2a0d3d72efa
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-17 18:24:24 -0500

    finished leet code problem 200

--- My-own-training-code/leet-code.js
+++ My-own-training-code/leet-code.js
@@ -76,12 +76,41 @@
 //  */
 
 
+const getAdjNeighbors = (i, j, grid, visited) => {
+    const adjNeighbors = [];
+
+    if (i > 0 && !visited[i - 1][j]) adjNeighbors.push([i - 1, j]);
+    if (i < grid.length - 1 && !visited[i + 1][j])adjNeighbors.push([i + 1, j]);
+
+    if (j > 0 && !visited[i][j - 1]) adjNeighbors.push([i,j -1]);
+    if (j < grid.length - 1 && !visited[i][j + 1])adjNeighbors.push([i, j + 1]);
+
+    return adjNeighbors;
+}
+
 const dFS = (i, j, grid, visited) => {
     const stack = [[i, j]];
     let islandSize = 0;
 
+    while(stack.length) {
+        let curNode = stack.pop();
 
-}
+        let [i, j] = curNode;
+        //check if visited at i and j;
+        if (visited[i][j]) continue;
+        visited[i][j] = true;
+
+        // check if cell is part of an island
+        if (grid[i][j] === "0") continue;
+        islandSize++;
+
+        let adjNeighbors = getAdjNeighbors(i, j, grid, visited);
+
+        stack.push(...adjNeighbors);
+    }
+    // this means "if islandSize is greater than 0 then return true (then return true = ?) else (else = :) return false"
+    return islandSize > 0 ? true : false;
+};
 
 const numIslands = function(grid) {
     const visited = grid.map(row => row.map(cell => false));
@@ -89,17 +118,13 @@ const numIslands = function(grid) {
 
 
     for(let i = 0; i < grid.length; i++) {
-        for(let j = 0; j < grid[j].length; j++) {
+        for(let j = 0; j < grid[i].length; j++) {
             if (dFS(i, j, grid, visited)) islandCount++;
         }
     }
 
 
-
-
-
-
-    return islandCount
+    return islandCount;
 
 };
 
