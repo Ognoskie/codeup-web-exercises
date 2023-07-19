3ed458ada7fa168dce1f76489e0ef38c5c8bd3fb
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-19 09:38:37 -0500

    added dom warm-ups

---
+++ js/dom-warmups.js
@@ -0,0 +1,8 @@
+const backgroundButton = document.getElementById('background-button');
+const body = document.getElementById('body')
+backgroundButton.addEventListener('click', () => {
+    body.style.backgroundColor = '#8F43EE';
+});
+
+
+
