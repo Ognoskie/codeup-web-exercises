76e8e6c96df43ac41262c0f756336712c2869151
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-21 16:00:43 -0500

    finished async and await

--- js/promises.js
+++ js/promises.js
@@ -61,37 +61,37 @@
 // Exercise .then
 
 
-const getEvents = (userName = "ognoskie") => {
-    const url = `https://api.github.com/users/${userName}/events`;
-    const options  = {
-        method: "GET",
-        headers: {
-            "Authorization": `token ${PROMISE_GITHUB_TOKEN}` ,
-        }
-    }
-    return fetch(url, options)
-        .then((response) => {
-            return response.json();
-        })
-        .catch(error => {
-            console.log(error.message)
-        })
-}
+// const getEvents = (userName = "ognoskie") => {
+//     const url = `https://api.github.com/users/${userName}/events`;
+//     const options  = {
+//         method: "GET",
+//         headers: {
+//             "Authorization": `token ${PROMISE_GITHUB_TOKEN}` ,
+//         }
+//     }
+//     return fetch(url, options)
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .catch(error => {
+//             console.log(error.message)
+//         })
+// }
 
 
-(() => { //IIFE (Immediately Invoked Function Expression)
-
-    getEvents("ognoskie").then((events) => {
-        console.log(events);
-        const pushEvents = events.filter(event => {
-            return event.type === "PushEvent"
-        });
-        console.log(pushEvents[0].created_at)
-    })
-
-
-})();
-
+// (() => { //IIFE (Immediately Invoked Function Expression)
+//
+//     getEvents("ognoskie").then((events) => {
+//         console.log(events);
+//         const pushEvents = events.filter(event => {
+//             return event.type === "PushEvent"
+//         });
+//         console.log(pushEvents[0].created_at)
+//     })
+//
+//
+// })();
+//
 
 
 
@@ -100,10 +100,34 @@ const getEvents = (userName = "ognoskie") => {
 
 
 
+const getEvents = async (userName = "ognoskie") => {
+    try{
+        const url = `https://api.github.com/users/${userName}/events`;
+        const options  = {
+            method: "GET",
+            headers: {
+                "Authorization": `token ${PROMISE_GITHUB_TOKEN}` ,
+            }
+        }
+        const response = await fetch(url, options);
+        const data = await response.json();
+        return data;
+    }catch (error) {
+        console.log(error);
+    }
+}
 
 
 
+(async () => { //IIFE (Immediately Invoked Function Expression)
 
+    const events = await getEvents("ognoskie");
+    console.log(events)
+    const pushEvents = events.filter(event => {
+        return event.type === "PushEvent"
+    })
+    console.log(pushEvents[0].created_at)
+})();
 
 
 
