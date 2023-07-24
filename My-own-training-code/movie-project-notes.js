9b1f3c6aaf847fc597d2e9bffa97021ddf04c1cc
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-24 18:30:57 -0500

    added movie-project-notes

---
+++ My-own-training-code/movie-project-notes.js
@@ -0,0 +1,101 @@
+"use strict";
+//////////////////////// global /////////////////////////////////////////
+const DOMAIN = "http://localhost:3000/movies";
+
+const getMovie = async (id) => {
+    const response = await fetch(`${DOMAIN}/${id}`);
+    const movie = await response.json();
+    return movie;
+}
+
+
+
+
+
+
+
+
+////////////////////// functions ///////////////////////////////////////
+
+
+const createMovie = async () => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify()
+    };
+    const response = await fetch(`${DOMAIN}`, options);
+    const apiResponse = await response.json();
+    return apiResponse;
+}
+
+
+
+
+
+
+const editMovie = async (id) => {
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify()
+    };
+    const response = await fetch(`${DOMAIN}/${id}`, options);
+    const apiResponse = response.json();
+    return apiResponse;
+};
+
+
+
+
+
+
+const deleteMovie = async (id) => {
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    const response = await fetch(`${DOMAIN}/${id}`, options);
+    const apiResponse = response.json();
+    return apiResponse;
+};
+
+
+
+
+const renderMovie = async (movie) => {
+    const card = document.createElement('div');
+    card.classList.add('movie-card');
+    card.innerHTML = `
+         <img src="" />
+        <h2>${movie.title}</h2>
+        <button class="btn delete">DELETE</button>
+    `;
+    const deleteBtn = card.querySelector('.btn.delete');
+    deleteBtn.addEventListener('click', async ()=>{
+        await deleteMovie(movie.id);
+        card.remove();
+    });
+    document.querySelector('#movies').appendChild(card);
+    return card;
+}
+
+
+// const renderEditMovie = async (movie) => {
+//     const
+// }
+
+
+
+
+///////////////////////////////// calling functions ///////////////////
+
+createMovie();
+
+renderMovie();
