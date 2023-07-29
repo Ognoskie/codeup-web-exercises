06f2ac3426cecb8ff64d983750dd1f1db12be3d3
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-28 21:03:08 -0500

    added dom assessment questions to practice

---
+++ My-own-training-code/js-practice.js
@@ -0,0 +1,74 @@
+
+
+const prefersSunMode = true;
+const prefersDarkMode = false;
+const whichMode = () => {
+    if ("") {
+        console.log('dark mode set');
+        document.body.style.background = 'black';
+    } else if (prefersSunMode) {
+        console.log('sun mode engaged')
+        document.body.style.background = 'orange';
+    }else {
+        console.log("its white")
+        document.body.style.background = 'white';
+    }
+}
+whichMode();
+
+
+
+//DOM assessment retry
+
+
+///////////////// retry //////////////
+
+// global
+const problemOne = document.getElementById('problem-1-p');
+const problemTwoBtn = document.getElementById('problem-2-btn');
+const bodyColor = document.querySelector('body');
+const problemTwoInput = document.getElementById('problem-2-input');
+const h4List = document.querySelectorAll('h4');
+const problemThreeBtn = document.getElementById('problem-3-btn');
+
+
+
+
+
+
+
+
+
+
+// function
+const problemOneDone = () => {
+    problemOne.innerHTML = "Problem one conquered"
+};
+const problemTwoDone = () => {
+    let input = problemTwoInput;
+    bodyColor.style.backgroundColor = input.value
+
+}
+const problemThreeDone = () => {
+
+}
+const problemEight = () => {
+    for (const h4 of h4List) {
+        h4.classList.toggle('problem-8-color')
+    }
+}
+
+
+
+
+
+
+
+// event listeners
+problemOne.addEventListener("click", problemOneDone)
+problemTwoBtn.addEventListener("click", problemTwoDone)
+problemThreeBtn.addEventListener('click', problemThreeDone)
+for (const h4 of h4List) {
+    h4.addEventListener('click', problemEight);
+}
+
