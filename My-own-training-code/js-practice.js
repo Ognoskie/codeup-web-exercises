4756c25ec925ae1ba55c0ce5ebcb370d26c16de6
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-01 14:15:30 -0500

    forgot = to fizz buzz problem

--- My-own-training-code/js-practice.js
+++ My-own-training-code/js-practice.js
@@ -100,7 +100,7 @@
 
 
 const fizzBuzz = () => {
-    for (let i = 1; i < 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("fizzbuzz")
         } else if (i % 5 === 0) {
